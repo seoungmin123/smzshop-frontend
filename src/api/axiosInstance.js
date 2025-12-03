890259e1e7f8635f434282ee3790cb6b749478a7
seoungmin123 <sm1111176@gmail.com>
2025-12-03 16:37:23 +0900

    fix : api 경로변경

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://localhost:8080/api/v1',
     timeout: 10000,
     headers: {'Content-Type': 'application/json'}
 });
